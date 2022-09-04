c82a67a6fb48e598920b93fec21b095cdba5cf89
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-09-04 20:13:48 +0300

    Three numbers of triangle

---
+++ task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить являются  три числа сторонами треугольника
+bool CheckTriangle(int a, int b, int c) =>a+b>c && b+c>a && a+c>b;
+
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Введите третье число: ");
+int number3 = int.Parse(Console.ReadLine()??"0");
+bool check = CheckTriangle(number1,number2,number3);
+Console.WriteLine(check? "Три числа являются сторонами треугольника":"Три числа не являются сторонами треугольника");
